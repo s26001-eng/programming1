18dd19b621fc4e4338a7da6db0352bd9a01916e1
s26001-eng <s26001@std.it-college.ac.jp>
2026-07-07 03:00:48 +0000

    2.10追加

---
+++ chap02/q2_10_1.py
@@ -0,0 +1,3 @@
+print(pow(*divmod(20, 3)))
+result = divmod(20, 3)
+print(pow(result[0], result[1]))
---
+++ chap02/q2_10_2.py
@@ -0,0 +1,3 @@
+sample_list = [2,3,4]
+print(list(map(str, sample_list)))
+print(list(map(int, '123')))
