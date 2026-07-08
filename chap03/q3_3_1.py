102b0035518cb28e35d871fd3a95088d219d42ed
s26001-eng <s26001@std.it-college.ac.jp>
2026-07-08 02:45:18 +0000

    3.3追加

---
+++ chap03/q3_3_1.py
@@ -0,0 +1,6 @@
+my_list = ['tokyo', 'osaka', 'fukuoka', 'aichi', 'kyoto', 'chiba', 'saitama', 'gunma']
+my_list_6 = []
+for s in my_list:
+    if len(s) >= 6:
+        my_list_6.append(s)
+print(my_list_6)
---
+++ chap03/q3_3_2.py
@@ -0,0 +1,6 @@
+my_list = [1, 1, 2, 3, 5, 8, 13]
+x = 0
+for n in my_list:
+    if n % 2 != 0:
+        x += n
+print(x)
