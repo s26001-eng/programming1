c22613f1ee6974461e4a6fab9d77c6663a31253e
s26001-eng <s26001@std.it-college.ac.jp>
2026-07-15 01:06:42 +0000

    4.1追加

---
+++ chap04/q4_1_2.py
@@ -0,0 +1,9 @@
+def fib2(n):
+    '''nより小さなフィボナッチ数列をリストで返す'''
+    result = []
+    a, b = 0,1
+    while a < n:
+        result.append(a)
+        a, b = b, a+b
+    return result
+print(fib2(1000)) 
