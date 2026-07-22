1fefbb8418f2dda8994c82260faffe5dbfef4e5b
s26001-eng <s26001@std.it-college.ac.jp>
2026-07-22 01:12:13 +0000

    q4_6_2.py変更

--- chap04/q4_6_2.py
+++ chap04/q4_6_2.py
@@ -21,15 +21,19 @@ def generate_students_data(num_students=10):
             print(i, name, height, weight)
         elif i == 2:
             print('...')
-        return students_data
+    return students_data
 
 students_data = generate_students_data(10)
 
-print(students_data)
+students_data
 
 students_by_height = sorted(students_data, key=lambda s: s[1])
-students_by_height = sorted(students_data, key=lambda s: s[2])
+students_by_weight = sorted(students_data, key=lambda s: s[2])
 
 print('\nSort by height')
 for student in students_by_height:
+    print(student)
+
+print('\nSort by weight')
+for student in students_by_weight:
     print(student)
